4de0fd71b5acaef9cb24f86f66c7a11cc3502c51
ijaihari <sjaiharinataraj@gmail.com>
2026-01-22 16:10:16 +0530

    project update

--- src/pages/ProjectsSection.jsx
+++ src/pages/ProjectsSection.jsx
@@ -4,6 +4,21 @@ import { ProjectCard } from "../components/ProjectCard";
 export function ProjectsSection() {
   const projects = [
     {
+      title: "Zomato Restaurant Data Analytics - Customer Behavior Analysis",
+      description:
+        " Analyzed Zomato restaurant data using Python to explore patterns in customer preferences, restaurant categories, pricing ranges, and ordering modes through exploratory data analysis",
+      tech: [
+        "Python",
+        "Pandas",
+        "NumPy",
+        "Matplotlib",
+        "Seaborn",
+        "EDA",        
+      ],
+      liveLink: "https://github.com/ijaihari/Zomato-Restaurant-Data-Analytics-Customer-Behavior-Analysis/blob/main/Zomato%20Restaurant%20Data%20Analytics%20-%20Customer%20Behavior%20Analysis.ipynb",
+      githubLink: "https://github.com/ijaihari/Zomato-Restaurant-Data-Analytics-Customer-Behavior-Analysis",
+    },
+    {
       title: "Tamil Nadu SIR 2026 – Voter Roll Impact Analysis",
       description:
         "Analyzed constituency-level voter roll changes using statistical methods and machine learning models to uncover patterns and data anomalies.",
