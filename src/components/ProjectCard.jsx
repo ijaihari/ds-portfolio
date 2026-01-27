301731e03796d1bb73e2c4b509b4c91c6eff1ae4
ijaihari <sjaiharinataraj@gmail.com>
2026-01-27 21:37:36 +0530

    styles update

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -76,9 +76,9 @@ export function ProjectCard({
                             className="
                 w-full
                 flex items-center justify-center gap-2
-                rounded-2xl
+                rounded-2xl px-4 py-2.5
             text-gray-800 border border-gray-300 bg-gray-100
-                px-4 py-2.5 hover:border-gray-800  transition-all duration-300
+                 hover:border-gray-800  transition-all duration-300
                 text-sm font-semibold hover:shadow-sm hover:backdrop-blur-md
               "
                         >
